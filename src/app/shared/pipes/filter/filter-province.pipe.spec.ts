89fae1647438b2474d4785d08caadc79f2c293f3
fedeparrilla10 <federicoivanpd@gmail.com>
2023-06-23 20:48:53 +0300

    Initial dist subtree commit

--- src/app/shared/pipes/filter/filter-province.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterProvincePipe } from './filter-province.pipe';
-
-describe('FilterProvincePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterProvincePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
